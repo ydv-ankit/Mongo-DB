b88bc7572835702a90499ed20f53f2830416c003
Ankit Ydv <ankityadav11241@gmail.com>
2023-05-03 23:43:57 +0530

    mongo connection

--- mongo.py
+++ mongo.py
@@ -1,5 +1,16 @@
 import pymongo
 
+# connects to mongoDB server
 myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 
 mydb = myclient["mydatabase"]
+
+# list available databases
+databases = myclient.list_database_names()
+
+# lets check if db "mydb" exists
+target_db = "mydb"
+if target_db in databases:
+    print(target_db + " exists")
+else:
+    print(target_db + " doesn't exists")
